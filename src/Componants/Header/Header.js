7a62e1cf7bfb5faa3f416bdd77b06304f0426d2f
munnisaikra <munnisaikra750@gmail.com>
2022-11-07 22:55:19 +0600

    added header title

--- src/Componants/Header/Header.js
+++ src/Componants/Header/Header.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <h2>Perfect Posture Club</h2>
+
+        <div className='container'>
+            <div><h2 className='text-rose-600 mt-10 p-10 text-2xl font-semibold'>Perfect Posture Club</h2></div>
+            <div className='logo'><img src='' alt=''></img></div>
         </div>
     );
 };
