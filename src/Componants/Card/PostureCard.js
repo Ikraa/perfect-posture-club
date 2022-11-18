a9bc017c874cdd34585e59cbce83ba62ac7e8989
munnisaikra <munnisaikra750@gmail.com>
2022-11-18 08:01:07 +0600

    complete

--- src/Componants/Card/PostureCard.js
+++ src/Componants/Card/PostureCard.js
@@ -2,22 +2,34 @@
 import React from "react";
 
 const PostureCard = (props) => {
-    // console.log(props.exercise.name);
-    const {name,details,startAge, toAge,unit,ageUnit,time,url,addToCart,exercise} = props.exercise;
+  // console.log(props.exercise.name);
+  const {
+    name,
+    details,
+    startAge,
+    toAge,
+    unit,
+    ageUnit,
+    time,
+    url,
+    addToCart,
+    exercise,
+  } = props.exercise;
   return (
     <div className="card   shadow-xl">
       <figure className="px-10 pt-10">
-        <img
-          src={url}
-          alt=""
-          className="rounded-xl"
-        />
+        <img src={url} alt="" className="rounded-xl" />
       </figure>
       <div className="card-body ">
         <h2 className="card-title text-black text-lg">{name}</h2>
         <p className="text-gray-500">{details}</p>
-        <p className="text-md font-bold text-black">For Age:  {startAge}-{toAge} <span className="text-green-600"> {ageUnit}</span></p>
-        <p className="text-md font-bold text-black">Time required: {time}    <span className="text-green-600"> {unit}</span></p>
+        <p className="text-md font-bold text-black">
+          For Age: {startAge}-{toAge}{" "}
+          <span className="text-green-600"> {ageUnit}</span>
+        </p>
+        <p className="text-md font-bold text-black">
+          Time required: {time} <span className="text-green-600"> {unit}</span>
+        </p>
         <div className="">
           <button
             onClick={() => {
--- src/Componants/ExerciseTime/Time.js
+++ src/Componants/ExerciseTime/Time.js
@@ -1,4 +1,4 @@
-//  Handle time set 
+//  Handle time-set 
   const Time = ({ item, setIsClick, click, setBreakTime, i}) => {
   const isMatch = localStorage.getItem("title");
   return (
