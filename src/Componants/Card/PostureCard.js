21c307dc6cf5fd86fbabb8c63162d98aad118f50
munnisaikra <munnisaikra750@gmail.com>
2022-11-18 17:50:06 +0600

    complete

--- src/Componants/Card/PostureCard.js
+++ src/Componants/Card/PostureCard.js
@@ -1,34 +1,22 @@
-//  Handle exercise card
+//  Handle exercise and card
 import React from "react";
 
-const PostureCard = (props) => {
-  // console.log(props.exercise.name);
-  const {
-    name,
-    details,
-    startAge,
-    toAge,
-    unit,
-    ageUnit,
-    time,
-    url,
-    addToCart,
-    exercise,
-  } = props.exercise;
+const PostureCard = ({ exercise, addToCart }) => {
   return (
     <div className="card   shadow-xl">
       <figure className="px-10 pt-10">
-        <img src={url} alt="" className="rounded-xl" />
+        <img src={exercise.url} alt="" className="rounded-xl" />
       </figure>
       <div className="card-body ">
-        <h2 className="card-title text-black text-lg">{name}</h2>
-        <p className="text-gray-500">{details}</p>
+        <h2 className="card-title text-black text-lg">{exercise.name}</h2>
+        <p className="text-gray-500">{exercise.details}</p>
         <p className="text-md font-bold text-black">
-          For Age: {startAge}-{toAge}{" "}
-          <span className="text-green-600"> {ageUnit}</span>
+          For Age: {exercise.startAge}-{exercise.toAge}
+          <span className="text-green-600"> {exercise.ageUnit}</span>
         </p>
         <p className="text-md font-bold text-black">
-          Time required: {time} <span className="text-green-600"> {unit}</span>
+          Time required: {exercise.time}{" "}
+          <span className="text-green-600"> {exercise.unit}</span>
         </p>
         <div className="">
           <button
